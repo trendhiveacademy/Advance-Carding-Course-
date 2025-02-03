d4f7d7d6546eb3e47f3f59efca2b62460bad6ba8
trendhiveacademy <trendhiveacademy@gmail.com>
2025-02-03 13:02:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,11 +23,15 @@ function onYouTubeIframeAPIReady() {
         width: "640",
         videoId: "i9fRQ7Nj5ZQ", // Your YouTube Video ID
         playerVars: {
-            "modestbranding": 1,  // Removes YouTube logo
-            "rel": 0,             // Disables recommended videos at the end
-            "showinfo": 0,        // Hides video info (deprecated, but still useful)
-            "controls": 1,        // Allows minimal controls
-            "disablekb": 1        // Disables keyboard shortcuts
+            "modestbranding": 1,   // Removes YouTube logo
+            "rel": 0,              // Disables recommended videos at the end
+            "showinfo": 0,         // Hides video info (deprecated, but still useful)
+            "controls": 1,         // Shows play/pause and volume controls
+            "disablekb": 1,        // Disables keyboard shortcuts
+            "iv_load_policy": 3,   // Hides annotations
+            "fs": 0,               // Disables fullscreen button
+            "cc_load_policy": 0,   // Hides captions
+            "modestbranding": 1    // Hides the "YouTube" logo
         }
     });
 }
